d7c827478a4fd609f7d30e01bd0a4b402f648ce6
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-20 11:19:07 -0700

    publish only one path to each item

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-pub mod token_as_literal;
-pub mod bracketed_expr;
-pub mod xhtml_expr;
-pub mod xhtml_class_child;
-pub mod xhtml_display_expr;
-pub mod xhtml_class_attr;
-pub mod xhtml_attr;
+mod token_as_literal;
+mod bracketed_expr;
+mod xhtml_expr;
+mod xhtml_class_child;
+mod xhtml_display_expr;
+mod xhtml_class_attr;
+mod xhtml_attr;
 pub mod xhtml;
 
