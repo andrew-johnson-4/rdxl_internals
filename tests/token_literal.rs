8595b6f60e7e594dcd33cf7cc6a5ad6e34164f79
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-20 12:28:40 -0700

    better test coverage

--- tests/token_literal.rs
+++ tests/token_literal.rs
@@ -151,141 +151,292 @@ fn token_literal25() {
    assert_eq!( t1.token_literal, "abstract" );
 }
 
-/*
-        } else if input.peek(Token![as]) {
-           let id: Token![as] = input.parse()?;
-           Ok(TokenAsLiteral::new("as".to_string(), id.span.clone()))
-        } else if input.peek(Token![become]) {
-           let id: Token![become] = input.parse()?;
-           Ok(TokenAsLiteral::new("become".to_string(), id.span.clone()))
-        } else if input.peek(Token![box]) {
-           let id: Token![box] = input.parse()?;
-           Ok(TokenAsLiteral::new("box".to_string(), id.span.clone()))
-        } else if input.peek(Token![break]) {
-           let id: Token![break] = input.parse()?;
-           Ok(TokenAsLiteral::new("break".to_string(), id.span.clone()))
-        } else if input.peek(Token![const]) {
-           let id: Token![const] = input.parse()?;
-           Ok(TokenAsLiteral::new("const".to_string(), id.span.clone()))
-        } else if input.peek(Token![continue]) {
-           let id: Token![continue] = input.parse()?;
-           Ok(TokenAsLiteral::new("continue".to_string(), id.span.clone()))
-        } else if input.peek(Token![crate]) {
-           let id: Token![crate] = input.parse()?;
-           Ok(TokenAsLiteral::new("crate".to_string(), id.span.clone()))
-        } else if input.peek(Token![do]) {
-           let id: Token![do] = input.parse()?;
-           Ok(TokenAsLiteral::new("do".to_string(), id.span.clone()))
-        } else if input.peek(Token![else]) {
-           let id: Token![else] = input.parse()?;
-           Ok(TokenAsLiteral::new("else".to_string(), id.span.clone()))
-        } else if input.peek(Token![enum]) {
-           let id: Token![enum] = input.parse()?;
-           Ok(TokenAsLiteral::new("enum".to_string(), id.span.clone()))
-        } else if input.peek(Token![extern]) {
-           let id: Token![extern] = input.parse()?;
-           Ok(TokenAsLiteral::new("extern".to_string(), id.span.clone()))
-        } else if input.peek(Token![final]) {
-           let id: Token![final] = input.parse()?;
-           Ok(TokenAsLiteral::new("final".to_string(), id.span.clone()))
-        } else if input.peek(Token![fn]) {
-           let id: Token![fn] = input.parse()?;
-           Ok(TokenAsLiteral::new("fn".to_string(), id.span.clone()))
-        } else if input.peek(Token![for]) {
-           let id: Token![for] = input.parse()?;
-           Ok(TokenAsLiteral::new("for".to_string(), id.span.clone()))
-        } else if input.peek(Token![if]) {
-           let id: Token![if] = input.parse()?;
-           Ok(TokenAsLiteral::new("if".to_string(), id.span.clone()))
-        } else if input.peek(Token![impl]) {
-           let id: Token![impl] = input.parse()?;
-           Ok(TokenAsLiteral::new("impl".to_string(), id.span.clone()))
-        } else if input.peek(Token![in]) {
-           let id: Token![in] = input.parse()?;
-           Ok(TokenAsLiteral::new("in".to_string(), id.span.clone()))
-        } else if input.peek(Token![let]) {
-           let id: Token![let] = input.parse()?;
-           Ok(TokenAsLiteral::new("let".to_string(), id.span.clone()))
-        } else if input.peek(Token![loop]) {
-           let id: Token![loop] = input.parse()?;
-           Ok(TokenAsLiteral::new("loop".to_string(), id.span.clone()))
-        } else if input.peek(Token![macro]) {
-           let id: Token![macro] = input.parse()?;
-           Ok(TokenAsLiteral::new("macro".to_string(), id.span.clone()))
-        } else if input.peek(Token![match]) {
-           let id: Token![match] = input.parse()?;
-           Ok(TokenAsLiteral::new("match".to_string(), id.span.clone()))
-        } else if input.peek(Token![mod]) {
-           let id: Token![mod] = input.parse()?;
-           Ok(TokenAsLiteral::new("mod".to_string(), id.span.clone()))
-        } else if input.peek(Token![move]) {
-           let id: Token![move] = input.parse()?;
-           Ok(TokenAsLiteral::new("move".to_string(), id.span.clone()))
-        } else if input.peek(Token![mut]) {
-           let id: Token![mut] = input.parse()?;
-           Ok(TokenAsLiteral::new("mut".to_string(), id.span.clone()))
-        } else if input.peek(Token![override]) {
-           let id: Token![override] = input.parse()?;
-           Ok(TokenAsLiteral::new("override".to_string(), id.span.clone()))
-        } else if input.peek(Token![priv]) {
-           let id: Token![priv] = input.parse()?;
-           Ok(TokenAsLiteral::new("priv".to_string(), id.span.clone()))
-        } else if input.peek(Token![pub]) {
-           let id: Token![pub] = input.parse()?;
-           Ok(TokenAsLiteral::new("pub".to_string(), id.span.clone()))
-        } else if input.peek(Token![ref]) {
-           let id: Token![ref] = input.parse()?;
-           Ok(TokenAsLiteral::new("ref".to_string(), id.span.clone()))
-        } else if input.peek(Token![return]) {
-           let id: Token![return] = input.parse()?;
-           Ok(TokenAsLiteral::new("return".to_string(), id.span.clone()))
-        } else if input.peek(Token![self]) {
-           let id: Token![self] = input.parse()?;
-           Ok(TokenAsLiteral::new("self".to_string(), id.span.clone()))
-        } else if input.peek(Token![Self]) {
-           let id: Token![Self] = input.parse()?;
-           Ok(TokenAsLiteral::new("Self".to_string(), id.span.clone()))
-        } else if input.peek(Token![static]) {
-           let id: Token![static] = input.parse()?;
-           Ok(TokenAsLiteral::new("static".to_string(), id.span.clone()))
-        } else if input.peek(Token![struct]) {
-           let id: Token![struct] = input.parse()?;
-           Ok(TokenAsLiteral::new("struct".to_string(), id.span.clone()))
-        } else if input.peek(Token![super]) {
-           let id: Token![super] = input.parse()?;
-           Ok(TokenAsLiteral::new("super".to_string(), id.span.clone()))
-        } else if input.peek(Token![trait]) {
-           let id: Token![trait] = input.parse()?;
-           Ok(TokenAsLiteral::new("trait".to_string(), id.span.clone()))
-        } else if input.peek(Token![type]) {
-           let id: Token![type] = input.parse()?;
-           Ok(TokenAsLiteral::new("type".to_string(), id.span.clone()))
-        } else if input.peek(Token![typeof]) {
-           let id: Token![typeof] = input.parse()?;
-           Ok(TokenAsLiteral::new("typeof".to_string(), id.span.clone()))
-        } else if input.peek(Token![unsafe]) {
-           let id: Token![unsafe] = input.parse()?;
-           Ok(TokenAsLiteral::new("unsafe".to_string(), id.span.clone()))
-        } else if input.peek(Token![unsized]) {
-           let id: Token![unsized] = input.parse()?;
-           Ok(TokenAsLiteral::new("unsized".to_string(), id.span.clone()))
-        } else if input.peek(Token![use]) {
-           let id: Token![use] = input.parse()?;
-           Ok(TokenAsLiteral::new("use".to_string(), id.span.clone()))
-        } else if input.peek(Token![virtual]) {
-           let id: Token![virtual] = input.parse()?;
-           Ok(TokenAsLiteral::new("virtual".to_string(), id.span.clone()))
-        } else if input.peek(Token![where]) {
-           let id: Token![where] = input.parse()?;
-           Ok(TokenAsLiteral::new("where".to_string(), id.span.clone()))
-        } else if input.peek(Token![while]) {
-           let id: Token![while] = input.parse()?;
-           Ok(TokenAsLiteral::new("while".to_string(), id.span.clone()))
-        } else if input.peek(Token![yield]) {
-           let id: Token![yield] = input.parse()?;
-           Ok(TokenAsLiteral::new("yield".to_string(), id.span.clone()))
-        } else {
-           let id: Ident = input.parse()?;
-           Ok(TokenAsLiteral::new(id.to_string(), id.span().clone()))
-        }
-*/
+#[test]
+fn token_literal26() {
+   let t1: TokenAsLiteral = parse_quote! { as };
+   assert_eq!( t1.token_literal, "as" );
+}
+
+#[test]
+fn token_literal27() {
+   let t1: TokenAsLiteral = parse_quote! { become };
+   assert_eq!( t1.token_literal, "become" );
+}
+
+#[test]
+fn token_literal28() {
+   let t1: TokenAsLiteral = parse_quote! { box };
+   assert_eq!( t1.token_literal, "box" );
+}
+
+#[test]
+fn token_literal29() {
+   let t1: TokenAsLiteral = parse_quote! { break };
+   assert_eq!( t1.token_literal, "break" );
+}
+
+#[test]
+fn token_literal30() {
+   let t1: TokenAsLiteral = parse_quote! { const };
+   assert_eq!( t1.token_literal, "const" );
+}
+
+#[test]
+fn token_literal31() {
+   let t1: TokenAsLiteral = parse_quote! { continue };
+   assert_eq!( t1.token_literal, "continue" );
+}
+
+#[test]
+fn token_literal32() {
+   let t1: TokenAsLiteral = parse_quote! { crate };
+   assert_eq!( t1.token_literal, "crate" );
+}
+
+#[test]
+fn token_literal33() {
+   let t1: TokenAsLiteral = parse_quote! { do };
+   assert_eq!( t1.token_literal, "do" );
+}
+
+#[test]
+fn token_literal34() {
+   let t1: TokenAsLiteral = parse_quote! { else };
+   assert_eq!( t1.token_literal, "else" );
+}
+
+#[test]
+fn token_literal35() {
+   let t1: TokenAsLiteral = parse_quote! { enum };
+   assert_eq!( t1.token_literal, "enum" );
+}
+
+#[test]
+fn token_literal36() {
+   let t1: TokenAsLiteral = parse_quote! { extern };
+   assert_eq!( t1.token_literal, "extern" );
+}
+
+#[test]
+fn token_literal37() {
+   let t1: TokenAsLiteral = parse_quote! { final };
+   assert_eq!( t1.token_literal, "final" );
+}
+
+#[test]
+fn token_literal38() {
+   let t1: TokenAsLiteral = parse_quote! { fn };
+   assert_eq!( t1.token_literal, "fn" );
+}
+
+#[test]
+fn token_literal39() {
+   let t1: TokenAsLiteral = parse_quote! { for };
+   assert_eq!( t1.token_literal, "for" );
+}
+
+#[test]
+fn token_literal40() {
+   let t1: TokenAsLiteral = parse_quote! { if };
+   assert_eq!( t1.token_literal, "if" );
+}
+
+#[test]
+fn token_literal41() {
+   let t1: TokenAsLiteral = parse_quote! { impl };
+   assert_eq!( t1.token_literal, "impl" );
+}
+
+#[test]
+fn token_literal42() {
+   let t1: TokenAsLiteral = parse_quote! { in };
+   assert_eq!( t1.token_literal, "in" );
+}
+
+#[test]
+fn token_literal43() {
+   let t1: TokenAsLiteral = parse_quote! { let };
+   assert_eq!( t1.token_literal, "let" );
+}
+
+#[test]
+fn token_literal44() {
+   let t1: TokenAsLiteral = parse_quote! { loop };
+   assert_eq!( t1.token_literal, "loop" );
+}
+
+#[test]
+fn token_literal45() {
+   let t1: TokenAsLiteral = parse_quote! { macro };
+   assert_eq!( t1.token_literal, "macro" );
+}
+
+#[test]
+fn token_literal46() {
+   let t1: TokenAsLiteral = parse_quote! { match };
+   assert_eq!( t1.token_literal, "match" );
+}
+
+#[test]
+fn token_literal47() {
+   let t1: TokenAsLiteral = parse_quote! { mod };
+   assert_eq!( t1.token_literal, "mod" );
+}
+
+#[test]
+fn token_literal48() {
+   let t1: TokenAsLiteral = parse_quote! { move };
+   assert_eq!( t1.token_literal, "move" );
+}
+
+#[test]
+fn token_literal49() {
+   let t1: TokenAsLiteral = parse_quote! { mut };
+   assert_eq!( t1.token_literal, "mut" );
+}
+
+#[test]
+fn token_literal50() {
+   let t1: TokenAsLiteral = parse_quote! { override };
+   assert_eq!( t1.token_literal, "override" );
+}
+
+#[test]
+fn token_literal51() {
+   let t1: TokenAsLiteral = parse_quote! { priv };
+   assert_eq!( t1.token_literal, "priv" );
+}
+
+#[test]
+fn token_literal52() {
+   let t1: TokenAsLiteral = parse_quote! { pub };
+   assert_eq!( t1.token_literal, "pub" );
+}
+
+#[test]
+fn token_literal53() {
+   let t1: TokenAsLiteral = parse_quote! { ref };
+   assert_eq!( t1.token_literal, "ref" );
+}
+
+#[test]
+fn token_literal54() {
+   let t1: TokenAsLiteral = parse_quote! { return };
+   assert_eq!( t1.token_literal, "return" );
+}
+
+#[test]
+fn token_literal55() {
+   let t1: TokenAsLiteral = parse_quote! { self };
+   assert_eq!( t1.token_literal, "self" );
+}
+
+#[test]
+fn token_literal56() {
+   let t1: TokenAsLiteral = parse_quote! { Self };
+   assert_eq!( t1.token_literal, "Self" );
+}
+
+#[test]
+fn token_literal57() {
+   let t1: TokenAsLiteral = parse_quote! { static };
+   assert_eq!( t1.token_literal, "static" );
+}
+
+#[test]
+fn token_literal58() {
+   let t1: TokenAsLiteral = parse_quote! { struct };
+   assert_eq!( t1.token_literal, "struct" );
+}
+
+#[test]
+fn token_literal59() {
+   let t1: TokenAsLiteral = parse_quote! { super };
+   assert_eq!( t1.token_literal, "super" );
+}
+
+#[test]
+fn token_literal60() {
+   let t1: TokenAsLiteral = parse_quote! { trait };
+   assert_eq!( t1.token_literal, "trait" );
+}
+
+#[test]
+fn token_literal61() {
+   let t1: TokenAsLiteral = parse_quote! { type };
+   assert_eq!( t1.token_literal, "type" );
+}
+
+#[test]
+fn token_literal62() {
+   let t1: TokenAsLiteral = parse_quote! { typeof };
+   assert_eq!( t1.token_literal, "typeof" );
+}
+
+#[test]
+fn token_literal63() {
+   let t1: TokenAsLiteral = parse_quote! { unsafe };
+   assert_eq!( t1.token_literal, "unsafe" );
+}
+
+#[test]
+fn token_literal64() {
+   let t1: TokenAsLiteral = parse_quote! { unsized };
+   assert_eq!( t1.token_literal, "unsized" );
+}
+
+#[test]
+fn token_literal65() {
+   let t1: TokenAsLiteral = parse_quote! { use };
+   assert_eq!( t1.token_literal, "use" );
+}
+
+#[test]
+fn token_literal66() {
+   let t1: TokenAsLiteral = parse_quote! { virtual };
+   assert_eq!( t1.token_literal, "virtual" );
+}
+
+#[test]
+fn token_literal67() {
+   let t1: TokenAsLiteral = parse_quote! { where };
+   assert_eq!( t1.token_literal, "where" );
+}
+
+#[test]
+fn token_literal68() {
+   let t1: TokenAsLiteral = parse_quote! { while };
+   assert_eq!( t1.token_literal, "while" );
+}
+
+#[test]
+fn token_literal69() {
+   let t1: TokenAsLiteral = parse_quote! { yield };
+   assert_eq!( t1.token_literal, "yield" );
+}
+
+#[test]
+fn token_literal70() {
+   let t1: TokenAsLiteral = parse_quote! { snake_case };
+   assert_eq!( t1.token_literal, "snake_case" );
+}
+
+#[test]
+fn token_literal71() {
+   let t1: TokenAsLiteral = parse_quote! { camel_case };
+   assert_eq!( t1.token_literal, "camel_case" );
+}
+
+#[test]
+fn token_literal72() {
+   let t1: TokenAsLiteral = parse_quote! { SCREAMING_SNAKE_CASE };
+   assert_eq!( t1.token_literal, "SCREAMING_SNAKE_CASE" );
+}
+
+#[test]
+#[should_panic]
+fn token_literal73() {
+   let t1: TokenAsLiteral = parse_quote! { SCREAMING_SNAKE_CASE };
+   assert_eq!( t1.token_literal, "SCREAMING_SNAKE_CASe" );
+}
+
