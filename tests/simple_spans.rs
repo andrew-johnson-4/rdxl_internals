5a02eae3b87b81c28c69b9a26ea7b25f2b2d7ebe
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-21 13:00:56 -0700

    2 possible ways to test, both panicking

--- tests/simple_spans.rs
+++ tests/simple_spans.rs
@@ -1,9 +1,24 @@
-use syn::{parse_quote,Token};
-
 /*
+use syn::{parse_quote,Token};
+use quote::quote;
+
+//assertion fails because LineColumn are default and equal
 #[test]
 fn span1() {
    let t: Token![!] = parse_quote! { ! };
+   println!("{:?}:{:?}-{:?}", t.span, t.span.start(), t.span.end());
+   assert_ne!(
+      t.span.start(),
+      t.span.end()
+   )
+}
+
+
+//panics with: procedural macro API is used outside of a procedural macro
+#[test]
+fn span2() {
+   let q = quote! { ! }.into();
+   let t: Token![!] = syn::parse(q).unwrap();
    println!("{:?}:{:?}-{:?}", t.span, t.span.start(), t.span.end());
    assert_ne!(
       t.span.start(),
